298d28aa4702663d0d9171ca52b84cee3078e7f4
Dom Barnes <dom@dombarnes.com>
2013-09-24 22:49:39 +0100

    New Downloads tracking
    - Additional code for tracking downloads instead of sales
    - Added some feedback when in Terminal
    - Added time to complete cos I wanted to know

--- salesboard.rb
+++ salesboard.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
-
+startTime = Time.now
 require 'rubygems'
 require 'httparty'
 require 'json/pure'
+require 'action_view'
+include ActionView::Helpers::DateHelper
 
 # SalesBoard - An AppFigures script for Status Board
 # Created by Justin Williams
@@ -10,35 +12,36 @@ require 'json/pure'
 # http://carpeaqua.com
 #
 # If you find this script useful, please consider purchasing one of my two products to show your support:
-# 
+#
 # * Elements for iOS : http://bit.ly/elements20
 # * Committed for OS X : http://bit.ly/committed10
 #
 # The README has more instructions on how to use this thing.
 
 ########################################
-# Configuration 
+# Configuration
 ########################################
 
 salesDays = 30 # Number of days you want to see.
-userName = "justinbieber@gmail.com"  # Your AppFigures username 
-password = "neversaynever" # Your App Figures password 
-currency = "$" # The selected currency under your AppFigures account settings
-graphTitle = "My Company's Sales" # The title for the graph
+userName = ""  # Your AppFigures username
+password = "" # Your App Figures password
+currency = "GBP" # The selected currency under your AppFigures account settings
+graphTitle = "My App Downloads" # The title for the graph
 graphType = "line" # This can be 'bar' or 'line'
-displayTotal = false # Set to true if you want a total revenue listed at the end of the graph.
-hideTotals = true # If you want to see the sales total for each day on the y-axis set this to true
+displayTotal = true # Set to true if you want a total revenue listed at the end of the graph.
+hideTotals = false # If you want to see the sales total for each day on the y-axis set this to true
 
-# This array contains a hash for each product. The :title should be your product name. 
+# This array contains a hash for each product. The :title should be your product name.
 # The :id is the App Figures product ID. You can fetch this at https://api.appfigures.com/v1.1/products/mine
 # The :color can be red, blue, green, yellow, orange, purple, aqua, or pink
-products = [ 
-    { :title => "Product 1", :id => 0000, :color => "green" }, 
-    { :title => "Product 2", :id => 0000, :color => "blue" } 
+products = [
+    { :title => "My First App", :id => 123455, :color => "green" },
+    { :title => "My Second App", :id => 123456, :color => "blue" }
+
 ]
 
 # Where you want to output the file on your computer. I recommend Dropbox since it can be publicly accessible.
-outputFile = "/Users/justin/Dropbox/Status\ Board/salesboard.json"
+outputFile = "/Users/username/Dropbox/Public/statusboard_data/salesboard.json"
 
 ########################################
 # The Guts
@@ -51,7 +54,7 @@ end
 
 startDate = (Date.today - salesDays).strftime("%Y-%m-%d")
 endDate = Time.now.strftime("%Y-%m-%d")
-months = { 
+months = {
     "1" => "Jan",
     "2" => "Feb",
     "3" => "Mar",
@@ -70,52 +73,55 @@ options = { :basic_auth => { :username => userName , :password => password } }
 datasequences = []
 minTotal = 0
 maxTotal = 1
+puts "== Fetching AppFigures Data    =="
 
-# Iterate through each product listed above. 
+# Iterate through each product listed above.
 products.each do |p|
 salesData = []
 response = HTTParty.get("https://api.appfigures.com/v1.1/sales/dates/#{startDate}/#{endDate}/?data_source=daily&products=#{p[:id]}", options)
-    response.parsed_response.each.sort.each do |day| 
+    response.parsed_response.each.sort.each do |day|
         # Parse the date into something nicely readable.
         date = Date.parse(day[1]["date"])
-        dateString = "#{months["#{date.month}"]} #{date.day}"        
+        dateString = "#{months["#{date.month}"]} #{date.day}"
 
         # We're rounding the sales data.
-        # Uncomment these lines to get sales revenue
-        revenue = comma_numbers(day[1]["revenue"].to_i)        
-        maxTotal = revenue.to_i if revenue.to_i > maxTotal
-        minTotal = revenue.to_i if revenue.to_i < minTotal || minTotal == 0
 
-        salesData << { :title => dateString, :value => revenue } 
+        # Uncomment these lines for sales revenue
+        # revenue = comma_numbers(day[1]["revenue"].to_i)
+        # maxTotal = revenue.to_i if revenue.to_i > maxTotal
+        # minTotal = revenue.to_i if revenue.to_i < minTotal || minTotal == 1
+        # salesData << { :title => dateString, :value => revenue }
 
-        # Uncomment these lines for downloads, and comment out line 111 to remove currency labels
-        # downloads = comma_numbers(day[1]["downloads"].to_i)
-        # maxTotal = downloads.to_i if downloads.to_i > maxTotal
-        # minTotal = downloads.to_i if downloads.to_i < minTotal || minTotal == 1
-
-        # salesData << { :title => dateString, :value => downloads } 
+        # Uncomment these lines for downloads
+        downloads = comma_numbers(day[1]["downloads"].to_i)
+        maxTotal = downloads.to_i if downloads.to_i > maxTotal
+        minTotal = downloads.to_i if downloads.to_i < minTotal || minTotal == 1
+        salesData << { :title => dateString, :value => downloads }
     end
 
     # Add the product to the data sequences.
     datasequences << { :title => p[:title], :color => p[:color], :datapoints => salesData }
-end 
-
+end
+puts "==        Generating Graphs    =="
 # This is where the graph is generated.
-salesGraph = { 
+salesGraph = {
     :graph =>  {
         :title => graphTitle,
         :total => displayTotal,
         :type => graphType,
         :yAxis => {
             "hide" => hideTotals,
-            :units => { :prefix => currency },
+            # :units => { :prefix => "Total " },
             :minValue => minTotal,
             :maxValue => maxTotal
-        },        
+        },
         :datasequences => datasequences
     }
 }
-
+puts "==           Updating Files    =="
 File.open(outputFile, "w") do |f|
   f.write(salesGraph.to_json)
 end
+puts "==                     Done    =="
+endTime = Time.now
+puts "==      Completed in #{((endTime - startTime)*1000.0).to_int}ms    =="
