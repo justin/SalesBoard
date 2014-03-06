92b40b9abe33def02d2110196c0106a6a52cf844
Dom Barnes <dom@dombarnes.com>
2014-03-06 22:51:04 +0000

    Updated to use AppFigures v2 API

--- salesboard.rb
+++ salesboard.rb
@@ -2,8 +2,11 @@
 startTime = Time.now
 require 'rubygems'
 require 'httparty'
-require 'json/pure'
+require 'base64'
+require 'date'
+require 'json'
 require 'action_view'
+require 'terminal-notifier'
 include ActionView::Helpers::DateHelper
 
 # SalesBoard - An AppFigures script for Status Board
@@ -22,26 +25,26 @@ include ActionView::Helpers::DateHelper
 # Configuration
 ########################################
 
-salesDays = 30 # Number of days you want to see.
+salesDays = 10 # Number of days you want to see.
 userName = ""  # Your AppFigures username
 password = "" # Your App Figures password
+clientKey =  "" # Your API Key from https://appfigures.com/account/api
 currency = "GBP" # The selected currency under your AppFigures account settings
-graphTitle = "My App Downloads" # The title for the graph
-graphType = "line" # This can be 'bar' or 'line'
+graphTitle = "" # The title for the graph
+graphType = "bar" # This can be 'bar' or 'line'
 displayTotal = true # Set to true if you want a total revenue listed at the end of the graph.
 hideTotals = false # If you want to see the sales total for each day on the y-axis set this to true
 
 # This array contains a hash for each product. The :title should be your product name.
-# The :id is the App Figures product ID. You can fetch this at https://api.appfigures.com/v1.1/products/mine
+# The :id is the App Figures product ID. You can fetch this at https://api.appfigures.com/v2/products/mine
 # The :color can be red, blue, green, yellow, orange, purple, aqua, or pink
 products = [
-    { :title => "My First App", :id => 123455, :color => "green" },
-    { :title => "My Second App", :id => 123456, :color => "blue" }
-
+    { :title => "App 1", :id => 12345677, :color => "green" },
+    { :title => "App 2", :id => 12345678, :color => "blue" }
 ]
 
 # Where you want to output the file on your computer. I recommend Dropbox since it can be publicly accessible.
-outputFile = "/Users/username/Dropbox/Public/statusboard_data/salesboard.json"
+outputFile = "/Users/myname/Dropbox/Public/statusboard_data/salesboard.json"
 
 ########################################
 # The Guts
@@ -53,7 +56,7 @@ def comma_numbers(number, delimiter = ',')
 end
 
 startDate = (Date.today - salesDays).strftime("%Y-%m-%d")
-endDate = Time.now.strftime("%Y-%m-%d")
+endDate = (Date.today - 1).strftime("%Y-%m-%d")
 months = {
     "1" => "Jan",
     "2" => "Feb",
@@ -68,60 +71,71 @@ months = {
     "11" => "Nov",
     "12" => "Dec"
 }
-
-options = { :basic_auth => { :username => userName , :password => password } }
 datasequences = []
 minTotal = 0
 maxTotal = 1
 puts "== Fetching AppFigures Data    =="
+lastDate = []
 
 # Iterate through each product listed above.
 products.each do |p|
-salesData = []
-response = HTTParty.get("https://api.appfigures.com/v1.1/sales/dates/#{startDate}/#{endDate}/?data_source=daily&products=#{p[:id]}", options)
-    response.parsed_response.each.sort.each do |day|
-        # Parse the date into something nicely readable.
-        date = Date.parse(day[1]["date"])
-        dateString = "#{months["#{date.month}"]} #{date.day}"
+    salesData = []
 
-        # We're rounding the sales data.
+    response = HTTParty.get("https://api.appfigures.com/v2/sales/products+dates/?start_date=#{startDate}&end_date=#{endDate}&granularity=daily&products=#{p[:id]}", :headers => { "X-Client-Key" => "#{clientKey}"}, :basic_auth => {:username => userName, :password => password })
 
-        # Uncomment these lines for sales revenue
-        # revenue = comma_numbers(day[1]["revenue"].to_i)
-        # maxTotal = revenue.to_i if revenue.to_i > maxTotal
-        # minTotal = revenue.to_i if revenue.to_i < minTotal || minTotal == 1
-        # salesData << { :title => dateString, :value => revenue }
+        response.parsed_response.each.sort.each do |day|
+            day_hash = day[1]
+            day_hash.each do |data|
+                # Parse the date into something nicely readable.
+                newDate = Date.parse(data[1]['date'])
+                dateString = "#{newDate.day} #{months["#{newDate.month}"]}"
+                # We're rounding the sales data.
 
-        # Uncomment these lines for downloads
-        downloads = comma_numbers(day[1]["downloads"].to_i)
-        maxTotal = downloads.to_i if downloads.to_i > maxTotal
-        minTotal = downloads.to_i if downloads.to_i < minTotal || minTotal == 1
-        salesData << { :title => dateString, :value => downloads }
+                # Uncomment these lines for sales revenue
+                # revenue = comma_numbers(day[1]["revenue"].to_i)
+                # maxTotal = revenue.to_i if revenue.to_i > maxTotal
+                # minTotal = revenue.to_i if revenue.to_i < minTotal || minTotal == 1
+                # salesData << { :title => dateString, :value => revenue }
+
+                # Uncomment these lines for downloads
+                downloads = comma_numbers(data[1]["downloads"].to_i)
+                maxTotal = downloads.to_i if downloads.to_i > maxTotal
+                minTotal = downloads.to_i if downloads.to_i < minTotal || minTotal == 1
+                salesData << { :title => dateString, :value => downloads }
+                lastDate = dateString
+            end
+        end
+        # Add the product to the data sequences.
+        datasequences << { :title => p[:title], :color => p[:color], :datapoints => salesData }
     end
-
-    # Add the product to the data sequences.
-    datasequences << { :title => p[:title], :color => p[:color], :datapoints => salesData }
-end
-puts "==        Generating Graphs    =="
-# This is where the graph is generated.
-salesGraph = {
-    :graph =>  {
-        :title => graphTitle,
-        :total => displayTotal,
-        :type => graphType,
-        :yAxis => {
-            "hide" => hideTotals,
-            # :units => { :prefix => "Total " },
-            :minValue => minTotal,
-            :maxValue => maxTotal
-        },
-        :datasequences => datasequences
+    puts "==        Generating Graphs    =="
+    # This is where the graph is generated.
+    salesGraph = {
+        :graph =>  {
+            :title => graphTitle,
+            :total => displayTotal,
+            :type => graphType,
+            :yAxis => {
+                "hide" => hideTotals,
+                # :units => { :prefix => "Total " },
+                :minValue => minTotal,
+                :maxValue => maxTotal
+            },
+            :datasequences => datasequences
+        }
     }
-}
-puts "==           Updating Files    =="
-File.open(outputFile, "w") do |f|
-  f.write(salesGraph.to_json)
+    puts "==           Updating Files    =="
+    File.open(outputFile, "w") do |f|
+      f.write(salesGraph.to_json)
 end
+
 puts "==                     Done    =="
 endTime = Time.now
+puts "== Publishing date upto #{lastDate} =="
 puts "==      Completed in #{((endTime - startTime)*1000.0).to_int}ms    =="
+
+#Send Notification
+message = ARGV[0] ||  "Salesboard Updated up to #{lastDate}"
+activate = 'com.googlecode.iterm2'
+# TerminalNotifier.notify(message, :activate => activate, :title => "Salesboard}")
+%x{/usr/bin/terminal-notifier -message "#{message}" -title "Salesboard" -activate #{activate}}
